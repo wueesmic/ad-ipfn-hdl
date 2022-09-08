0592ccffc7fcebfd748b0755f73504c23b30f003
Bernardo Carvalho <bernardo@debian-BULLSEYE-live-builder-AMD64>
2022-09-08 18:57:51 +0100

    updating to v 2012.1
    Signed-off-by: Bernardo Carvalho <bernardo@debian-BULLSEYE-live-builder-AMD64>

--- projects/fmcjesdadc1/kc705/system_project.tcl
+++ projects/fmcjesdadc1/kc705/system_project.tcl
@@ -25,13 +25,21 @@ adi_project fmcjesdadc1_kc705 0 [list \
   RX_JESD_NP   [get_env_param RX_JESD_NP   16] \
 ]
 
+#  "system_top.v" 
 adi_project_files fmcjesdadc1_kc705 [list \
   "../common/fmcjesdadc1_spi.v" \
-  "system_top.v" \
   "system_constr.xdc" \
+  "./src/hdl/system_top.sv" \
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
   "$ad_hdl_dir/library/common/ad_sysref_gen.v" \
-  "$ad_hdl_dir/projects/common/kc705/kc705_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/kc705/kc705_system_constr.xdc" \
+  "src/constraints/kc705_sma_constr.xdc" \
+  "src/ip/xdma_0/xdma_0.xci" \
+  "src/hdl/shapi_regs_v1.sv" \
+  "src/hdl/shapi_stdrt_dev_inc.vh" \
+  "src/constraints/pcie_xdma_kc705_x4g2.xdc" \
+  "src/hdl/trigger_gen.v" ]
 
-adi_project_run fmcjesdadc1_kc705
+# Uncomment  to implement project with make ... (takes time)
+# adi_project_run fmcjesdadc1_kc705
 
